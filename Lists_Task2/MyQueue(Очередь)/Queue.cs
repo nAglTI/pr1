a29a9576e7fe322fee98c6c095623ff55311cc87
Daniil Burmich <33153197+nAglTI@users.noreply.github.com>
2020-03-27 14:39:59 +0300

    Очередь через двусвязный список

---
+++ Lists_Task2/MyQueue(Очередь)/Queue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue_Очередь_
+{
+    class Queue<T> where T : IEquatable<T>
+    {
+        private QueueList2<T> list;
+
+        public Queue()
+        {
+
+        }
+
+        public void Enqueue(T x)
+        {
+            list.AddLast(x);
+        }
+
+        public T Dequeue()
+        {
+            return list.First();
+        }
+
+        public T Peek()
+        {
+            return list.Peek();
+        }
+
+        public int Size()
+        {
+            return list.Size();
+        }
+    }
+}
---
+++ Lists_Task2/MyQueue(Очередь)/QueueList2.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyQueue_Очередь_
+{
+    class QueueList2<T> where T : IEquatable<T>
+    {
+        public Node2<T> head;
+        public Node2<T> tail;
+        public int Count;
+        public T this[int index]
+        {
+            get
+            {
+                Node2<T> current = head;
+                for (int i = 0; i != index && current != null; i++)
+                    current = current.Next;
+                return current.Value;
+
+            }
+            set
+            {
+                Node2<T> current = head;
+                for (int i = 0; i != index && current != null; i++)
+                    current = current.Next;
+                current.Value = value;
+            }
+        }
+
+        public QueueList2()
+        {
+
+        }
+
+        public T First()
+        {
+            T num = head.Value;
+            Remove(0);
+            return num;
+        }
+        public T Peek()
+        {
+            return head.Value;
+        }
+        public T Last()
+        {
+            return tail.Value;
+        }
+        
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            Count = 0;
+        }
+
+        public int Size()
+        {
+            return Count;
+        }
+
+        public void AddLast(T x)
+        {
+            var toAdd = new Node2<T>(x);
+            
+            if (head == null)
+            {
+                head = toAdd;
+            }
+            else
+            {
+                tail.Next = toAdd;
+                toAdd.Previous = tail;
+            }
+            tail = toAdd;
+            Count++;
+        }
+
+        public void Remove(int index)
+        {
+            if (head != null)
+            {
+                if (index == 0)
+                {
+                    head = head.Next;
+                }
+                else if (index == Count - 1)
+                {
+                    tail = tail.Previous;
+                }
+                else if (index < Count - 1)
+                {
+                    Node2<T> current = head;
+                    for (int i = 0; i < index - 1 && current != null; i++)
+                    {
+                        current = current.Next;
+                    }
+                    current.Next.Previous = current.Previous;
+                    current.Previous.Next = current.Next;
+                }
+                Count--;
+            }
+        }
+
+    }
+}
