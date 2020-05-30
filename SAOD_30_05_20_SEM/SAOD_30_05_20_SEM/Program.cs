75bf606ac367f1a28b61bfea0578f0e3da0892d5
Daniil Burmich <33153197+nAglTI@users.noreply.github.com>
2020-05-30 14:15:57 +0300

    Добавил бинарный поиск

--- SAOD_30_05_20_SEM/SAOD_30_05_20_SEM/Program.cs
+++ SAOD_30_05_20_SEM/SAOD_30_05_20_SEM/Program.cs
@@ -21,8 +21,57 @@ namespace SAOD_30_05_20_SEM
             Console.WriteLine(llist.IndexOf(1488));
             Console.WriteLine(llist.IndexOf(2));
             Console.WriteLine(llist.IndexOf(-1));
+
+            int[] arr = new int[100];
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                arr[i] = rnd.Next(0, 101);
+            }
+
+            int temp;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+
+            InsertBinarySearch(13);
+
+            void InsertBinarySearch(int val)
+            {
+                Array.Resize(ref arr, arr.Length + 1);
+                int index = BinarySearch(arr, val, 0, arr.Length - 1);
+                for (int i = arr.Length - 2; i > index; i--)
+                    arr[i] = arr[i + 1];
+
+                arr[index] = val;
+            }
+
+        }
+
+        public static int BinarySearch(int[] array, int val, int left, int right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (array[mid] < val && array[mid] >= val)
+                return mid + 1;
+
+            else if (array[mid] > val)
+                return BinarySearch(array, val, left, mid);
+            else
+                return BinarySearch(array, val, mid + 1, right);
         }
     }
+
+
 
     public static class IndexOfnerGGWP
     {
