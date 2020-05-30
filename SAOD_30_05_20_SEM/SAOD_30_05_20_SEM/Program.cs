e83d57b18f9e3ded6e4c6388fb886be109dd8c01
Daniil Burmich <33153197+nAglTI@users.noreply.github.com>
2020-05-30 13:25:36 +0300

    Метод расширения IndexOf для LinkedList

---
+++ SAOD_30_05_20_SEM/SAOD_30_05_20_SEM/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAOD_30_05_20_SEM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var llist = new LinkedList<int>();
+
+            llist.AddLast(1488);
+            llist.AddLast(228);
+            llist.AddLast(3);
+            llist.AddLast(666);
+
+            Console.WriteLine(llist.IndexOf(228));
+            Console.WriteLine(llist.IndexOf(1488));
+            Console.WriteLine(llist.IndexOf(2));
+            Console.WriteLine(llist.IndexOf(-1));
+        }
+    }
+
+    public static class IndexOfnerGGWP
+    {
+        public static int IndexOf<T>(this LinkedList<T> FList, T val)
+        {
+            int index = 0;
+
+            foreach (T value in FList)
+            {
+                if (value.Equals(val))
+                {
+                    return index;
+                }
+
+                index++;
+            }
+            return -1;
+        }
+    }
+}
